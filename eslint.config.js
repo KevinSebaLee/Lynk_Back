567bc345493bcb1fba32553a6c88823bceedacdf
KEVIN SEBASTIAN LEE <48044115@ort>
2025-08-05 11:30:43 -0300

    fix: Error when executing action

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,12 @@
 import js from '@eslint/js';
-import nodePlugin from 'eslint-plugin-node';
 
 export default [
   js.configs.recommended,
   {
-    plugins: {
-      node: nodePlugin,
-    },
+    ignores: ['node_modules/**', 'dist/**'],
     rules: {
-      ...nodePlugin.configs['recommended'].rules,
       'quotes': ['error', 'single'],
       'semi': ['error', 'always'],
     },
-    ignores: ['node_modules/**', 'dist/**'],
   },
 ];
